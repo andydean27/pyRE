4156ed638b0880f21c3eb02ba2dd7ffcccab9545
Andrew Dean <andydean27@gmail.com>
2025-06-06 15:02:38 +0100

    Enhance docstrings for Rock and Coal classes to include detailed argument descriptions

--- pyRE/rock/models.py
+++ pyRE/rock/models.py
@@ -11,6 +11,14 @@ class Rock:
             density: float,
             compressibility: float
             ):
+        """
+        Initialises the Rock class with basic properties.
+        Arguments:
+            - porosity : float : Porosity of the rock (fraction)
+            - permeability : float : Permeability of the rock (mD)
+            - density : float : Density of the rock (g/cm3)
+            - compressibility : float : Compressibility of the rock (1/psi)
+        """
         self.porosity = porosity
         self.permeability = permeability
         self.density = density
@@ -34,10 +42,6 @@ class Coal(Rock):
     Rock properties for coal seam reservoirs.
     This class inherits from the Rock class and adds specific properties for coal.
 
-    Arguments
-        gas_content : float : Gas content in scf/ton
-        langmuir_pressure : float : Langmuir pressure in psia
-        langmuir_volume : float : Langmuir volume in scf/ton (dry ash-free basis)
     """
     def __init__(
             self, 
@@ -49,6 +53,20 @@ class Coal(Rock):
             *args,
             **kwargs, 
             ):
+        """
+        Initialises the Coal class with specific properties for coal seam reservoirs.
+
+        Arguments
+            - porosity : float : Porosity of the rock (fraction)
+            - permeability : float : Permeability of the rock (mD)
+            - density : float : Density of the rock (g/cm3)
+            - compressibility : float : Compressibility of the rock (1/psi)
+            - gas_content : float : Gas content in scf/ton
+            - langmuir_pressure : float : Langmuir pressure in psia
+            - langmuir_volume : float : Langmuir volume in scf/ton (dry ash-free basis)
+            - ash_fraction : float : Ash fraction (default is 0)
+            - moisture_fraction : float : Moisture fraction (default is 0)
+        """
         super().__init__(*args, **kwargs)
 
         self.gas_content = gas_content  # Gas content in scf/ton
